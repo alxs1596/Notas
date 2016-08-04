089f5dcee82bc25624c35d2cae49c2de4db7759c
Gianella Milón <Gianella Miln@DESKTOP-7NEUNUN>
2016-08-03 20:05:45 -0500

    Commit de Mineiko

--- src/java/beans/Clase.java
+++ src/java/beans/Clase.java
@@ -10,8 +10,31 @@ package beans;
  * @author alexis
  */
 public class Clase {
-
+        String a;
+        int b;
     public Clase() {
+        
+        
+        
+        
     }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+    
+    
     
 }
